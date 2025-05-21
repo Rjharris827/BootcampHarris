85f20bfe65e598f24a142cd3d851b7e17c595507
Rjharris827 <robertjoshua827@gmail.com>
2025-05-21 11:34:08 -0400

    Update LeaseContract.java

--- CarDealer/src/main/java/org/example/LeaseContract.java
+++ CarDealer/src/main/java/org/example/LeaseContract.java
@@ -1,4 +1,22 @@
 package org.example;
 
-public class LeaseContract {
-}
+
+    public class LeaseContract extends Contract {
+        public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+            super(date, customerName, customerEmail, vehicle);
+        }
+
+        @Override
+        public double getTotalPrice() {
+            double expectedEndingValue = getVehicle().getPrice() * 0.50;
+            double leaseFee = getVehicle().getPrice() * 0.07;
+            return expectedEndingValue + leaseFee;
+        }
+
+        @Override
+        public double getMonthlyPayment() {
+            double leaseAmount = getTotalPrice();
+            return (leaseAmount * 1.04) / 36; // 4% over 36 months
+        }
+    }
+
