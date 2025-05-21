64923dbdd1f99fc676ee5bf66ba6cf68ba5dc45f
Rjharris827 <robertjoshua827@gmail.com>
2025-05-21 11:30:28 -0400

    Update Contract.java

--- CarDealer/src/main/java/org/example/Contract.java
+++ CarDealer/src/main/java/org/example/Contract.java
@@ -1,4 +1,35 @@
 package org.example;
 
-public class Contract {
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicle;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicle = vehicle;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    // Abstract methods must be implemented by subclasses
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
